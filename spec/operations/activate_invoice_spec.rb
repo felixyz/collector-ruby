8f3fd23c174c49e4b5051e768cd42d54614d8347
Felix Holmgren <felix.holmgren@gmail.com>
2013-07-26 14:57:20 +0200

    [FIX] Spec failing due to type error.

--- spec/operations/activate_invoice_spec.rb
+++ spec/operations/activate_invoice_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 require 'vcr'
-require 'webmock/rspec'
-require 'nori'  # Available through Savon
 
 describe "Collector::Client#activate_invoice" do
   before :all do
@@ -22,8 +20,8 @@ describe "Collector::Client#activate_invoice" do
       response.payment_reference.should_not be_nil
       response.lowest_amount_to_pay.should eq "24.00"
       response.total_amount.should eq "24.00"
-      expected_due_date = Time.now + 180*24*60*60
-      response.due_date.to_time.should be_within(24*60*60).of(expected_due_date)
+      expected_due_date = (Time.now + 180*24*60*60).to_f
+      response.due_date.to_time.to_f.should be_within(24*60*60).of(expected_due_date)
       response.invoice_url.should match %r(https://commerce.collector.se/testportal/PdfInvoice\?pnr=(\d+)\&invnr=#{@invoice_no})
     end
   end
